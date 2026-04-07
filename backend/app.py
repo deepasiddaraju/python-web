1053c37a2a06fecdcc9ed92569c2d150f37d16a7
deepa siddaraju <sushmitha.3285@gmail.com>
2026-04-07 16:07:32 +0530

    Added html and css and navbar

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,35 @@
-from flask import Flask
-app=Flask(__name__)
+from flask import Flask, render_template,jsonify
+app=Flask(__name__,template_folder="../frontend/template", static_folder="../frontend/static")
+JOBS=[
+  {"id":1,
+   "title":"Data Analyst",
+   "location":"Bengalore",
+   "salary":"Rs.10,00,000"
+   },
+   {"id":2,
+   "title":"Data Science",
+   "location":"Mysore",
+   "salary":"Rs.50,000"
+   },
+   {"id":3,
+   "title":"Data Engineer",
+   "location":"Belagavi",
+   "salary":"Rs.20,00,000"
+   },
+   {"id":4,
+   "title":"Full stack",
+   "location":"Bengalore",
+   
+   }
+]
+
 @app.route("/")
 def hello_world():
-  return "hello world"
+  return render_template('home.html',jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
 
